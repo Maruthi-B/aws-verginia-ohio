59ac937946f6e6fa855962410d8d2c258222c6ac
Maruthi-B <marutiyadav4524@gmail.com>
2020-11-12 08:58:19 +0000

    List of buckets in s3

--- simple.py
+++ simple.py
@@ -1,4 +1,7 @@
 import boto3
 session = boto3.Session(profile_name="Maruthi")
-s3-client = session.client(service_name="s3")
+s3_client = session.client(service_name="s3")
+for each_bucket in s3_client.list_buckets():
+    print(each_bucket.get('Name')
+
 
